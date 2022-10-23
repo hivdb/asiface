9112b8ded15163aabc103d69c1339cc87c57d935
Philip Tzou <philip.npc@gmail.com>
2022-10-23 14:06:59 -0700

    Add auto-save to ASIEditor

--- src/asiface.js
+++ src/asiface.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import createLocationState from 'icosa/utils/use-location-state';
+import createPersistedState from 'use-persisted-state/src';
 
-import Header, {useFetchAndSet} from './header';
+import Header from './header';
 import XMLEditor from './xml-editor';
 import MutationEditor from './mutation-editor';
 import Evaluator from './evaluator';
@@ -12,6 +13,15 @@ import style from './style.module.scss';
 
 const useVerticalPcnt = createLocationState('vertical-pcnt');
 const useHorizontalPcnt = createLocationState('horizontal-pcnt');
+const useAsiXml = createPersistedState('asi-xml');
+const useAsiFileName = createPersistedState('asi-file-name');
+const useAutoSave = createPersistedState('asi-auto-save');
+
+
+function getFileName(url) {
+  return url.split('/').slice(-1)[0];
+}
+
 
 ASIFace.propTypes = {
   config: PropTypes.object.isRequired,
@@ -24,18 +34,65 @@ ASIFace.defaultProps = {
 
 export default function ASIFace({height, config}) {
   const [defaultPreload] = config.preloads;
-  const [asiXml, setAsiXml] = React.useState(null);
   const [mutations, setMutations] = React.useState(config.mutations);
   const [verticalPcnt, setVerticalPcnt] = useVerticalPcnt(0.3);
   const [horizontalPcnt, setHorizontalPcnt] = useHorizontalPcnt(0.618);
   const [mobilePcnt1, setMobilePcnt1] = useVerticalPcnt(0.3);
   const [mobilePcnt2, setMobilePcnt2] = useHorizontalPcnt(0.5);
 
-  const fetchAndSet = useFetchAndSet(setAsiXml);
+  const [autoSave, setAutoSave] = useAutoSave(true);
+  const [asiXml, setAsiXml] = useAsiXml(null);
+  const [asiFileName, setAsiFileName] = useAsiFileName(null);
+
+  const mounted = React.useRef();
 
   React.useEffect(
-    () => fetchAndSet(defaultPreload.url),
-    [defaultPreload.url, fetchAndSet]
+    () => {
+      mounted.current = true;
+      return () => mounted.current = false;
+    },
+    []
+  );
+
+  const fetchAndSet = React.useCallback(
+    async (url) => {
+      const resp = await fetch(url);
+      const xml = await resp.text();
+      if (mounted.current) {
+        setAsiXml(xml);
+        setAsiFileName(getFileName(url));
+      }
+    },
+    [setAsiXml, setAsiFileName]
+  );
+
+  React.useEffect(
+    () => {
+      if (!autoSave || !asiXml) {
+        fetchAndSet(defaultPreload.url);
+      }
+    },
+    [/* eslint-disable-line react-hooks/exhaustive-deps */]
+  );
+
+  const handleChange = React.useCallback(
+    ({url, asiXml, asiFileName, autoSave}) => {
+      if (url) {
+        fetchAndSet(url);
+      }
+      else if (autoSave !== undefined) {
+        setAutoSave(autoSave);
+      }
+      else {
+        if (asiXml !== undefined) {
+          setAsiXml(asiXml);
+        }
+        if (asiFileName !== undefined) {
+          setAsiFileName(asiFileName);
+        }
+      }
+    },
+    [fetchAndSet, setAsiXml, setAsiFileName, setAutoSave]
   );
 
   return (
@@ -49,10 +106,14 @@ export default function ASIFace({height, config}) {
        '--mobile-pcnt-2': `${mobilePcnt2 * 100}%`
      }}>
       <Header
+       autoSave={autoSave}
        asiXml={asiXml}
+       asiFileName={asiFileName}
        preloads={config.preloads}
-       onChange={setAsiXml} />
-      <XMLEditor onChange={setAsiXml}>{asiXml}</XMLEditor>
+       onChange={handleChange} />
+      <XMLEditor onChange={setAsiXml}>
+        {asiXml}
+      </XMLEditor>
       <MutationEditor onChange={setMutations}>{mutations}</MutationEditor>
       <Evaluator asiXml={asiXml} mutations={mutations} />
       <ResizeBar
--- src/header.js
+++ src/header.js
@@ -4,12 +4,19 @@ import Select from 'icosa/components/select';
 import readFile from 'icosa/utils/read-file';
 import FileInput from 'icosa/components/file-input';
 import Button from 'icosa/components/button';
+import CheckboxInput from 'icosa/components/checkbox-input';
 import {makeDownload} from 'icosa/utils/download';
 
 import {ReactComponent as Logo} from './logo.svg';
 import style from './style.module.scss';
 
-export function useFetchAndSet(setValue) {
+
+function getFileName(url) {
+  return url.split('/').slice(-1)[0];
+}
+
+
+export function useFetchAndSet(setAsiPayload) {
   const mounted = React.useRef();
 
   React.useEffect(
@@ -24,20 +31,21 @@ export function useFetchAndSet(setValue) {
     url => {
       fetch(url)
         .then(resp => resp.text()
-          .then(payload => mounted.current && setValue(payload)));
+          .then(xml => mounted.current && setAsiPayload({
+            xml,
+            fileName: getFileName(url)
+          })));
     },
-    [setValue]
+    [setAsiPayload]
   );
 
 }
 
-function getFileName(url) {
-  return url.split('/').slice(-1)[0];
-}
-
 
 Header.propTypes = {
+  autoSave: PropTypes.bool.isRequired,
   asiXml: PropTypes.string,
+  asiFileName: PropTypes.string,
   preloads: PropTypes.arrayOf(
     PropTypes.shape({
       family: PropTypes.string.isRequired,
@@ -49,13 +57,15 @@ Header.propTypes = {
   onChange: PropTypes.func.isRequired
 };
 
-export default function Header({asiXml, preloads, onChange}) {
+export default function Header({
+  autoSave,
+  asiXml,
+  asiFileName,
+  preloads,
+  onChange
+}) {
 
   const mounted = React.useRef();
-  const [defaultPreload] = preloads;
-  const [lastSelected, setLastSelected] = React.useState(
-    getFileName(defaultPreload.url)
-  );
 
   const options = React.useMemo(
     () => preloads ? preloads.map(
@@ -85,16 +95,6 @@ export default function Header({asiXml, preloads, onChange}) {
     [options]
   );
 
-  const fetchAndSet = useFetchAndSet(onChange);
-
-  const handleChange = React.useCallback(
-    opt => {
-      fetchAndSet(opt.url);
-      setLastSelected(opt.url);
-    },
-    [fetchAndSet]
-  );
-
   const handleFileSelect = React.useCallback(
     ([file]) => {
       if (
@@ -106,17 +106,26 @@ export default function Header({asiXml, preloads, onChange}) {
       readFile(file)
         .then(xml => {
           if (mounted.current) {
-            onChange(xml);
-            setLastSelected(file.name);
+            onChange({
+              xml,
+              asiFileName: file.name
+            });
           }
         });
     },
     [onChange]
   );
 
+  const handleToggleAutoSave = React.useCallback(
+    event => onChange({
+      autoSave: event.currentTarget.checked
+    }),
+    [onChange]
+  );
+
   const handleSave = React.useCallback(
-    () => makeDownload(lastSelected, 'application/xml', asiXml),
-    [lastSelected, asiXml]
+    () => makeDownload(asiFileName, 'application/xml', asiXml),
+    [asiXml, asiFileName]
   );
 
   React.useEffect(
@@ -137,7 +146,7 @@ export default function Header({asiXml, preloads, onChange}) {
        name="asi-preload"
        classNamePrefix="asi-preload-dropdown"
        placeholder="Select a preloaded XML"
-       onChange={handleChange} />
+       onChange={onChange} />
       <span className={style.or}>or</span>
       <FileInput
        name="asi-custom"
@@ -151,6 +160,13 @@ export default function Header({asiXml, preloads, onChange}) {
        className={style['asi-save']}>
         Save XML
       </Button> : null}
+      <CheckboxInput
+       id="asi-auto-save"
+       className={style['asi-auto-save-checkbox']}
+       onChange={handleToggleAutoSave}
+       checked={autoSave}>
+        Auto save
+      </CheckboxInput>
     </header>
   );
 }
